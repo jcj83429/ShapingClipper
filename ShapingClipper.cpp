56645e22c79d40db2d88cd1b10fb1a3b3ca735b2
jcj83429 <jcj83429@gmail.com>
2021-07-04 15:46:45 -0700

    fix marginCurveShift on linux
    used std::abs to avoid being converted to int

--- ShapingClipper.cpp
+++ ShapingClipper.cpp
@@ -74,7 +74,7 @@ void ShapingClipper::feed(const double* inSamples, double* outSamples){
 
     peak = 0;
     for(int i=0; i<this->size; i++)
-      peak = std::max<double>(peak, abs((windowedFrame[i] + clippingDelta[i]) * invWindow[i]));
+      peak = std::max<double>(peak, std::abs((windowedFrame[i] + clippingDelta[i]) * invWindow[i]));
 
     double maskCurveShift = std::max<double>(Aquila::dB(peak), 1.0);
 
